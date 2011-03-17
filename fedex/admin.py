6994b00b433f9598678d8cb0c68e5543f4c233a9
Jaie Wilson <jaiew@helixta.com.au>
2011-03-17 21:35:50 +1100

    added more admin models.

--- fedex/admin.py
+++ fedex/admin.py
@@ -1,19 +1,41 @@
 from django.contrib import admin
-from fedex.models import OtterUser
-from django.contrib.auth.models import User
+from fedex.models import *
+from django.contrib.auth.models import * 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
+class AgencyAdmin(admin.ModelAdmin):
+    pass
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+class RevisionAdmin(admin.ModelAdmin):
+    pass
+
+class FileAdmin(admin.ModelAdmin):
+    pass
+
+class RegionAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(OtterUser, UserAdmin)
+admin.site.register(Agency, AgencyAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Revision, RevisionAdmin)
+admin.site.register(File, FileAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Comment, CommentAdmin)
 
 adminUser = User.objects.get(username__exact='admin')
-print adminUser
 if not adminUser:
     User.objects.create_user('admin', 'admin@otter.com', 'admin')
     adminUser.is_staff = True
     adminUser.save()
-    print "asdasd"
 else:
     adminUser.set_password('admin')
     adminUser.save()	
