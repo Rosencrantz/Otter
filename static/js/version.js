cd1311b71c9575f2404020877cd01de848f188f6
jaredwy <jared.wyles@gmail.com>
2011-03-17 23:40:13 +1100

    versions all hooked up ready to go

---
+++ static/js/version.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".version-item").click(function(e) {
+        var val = $(this).text();
+        e.preventDefault();
+        
+        
+        
+    });
+});
