ba8f9cf5b884160a6cfe8f118904c29020cd0e1c
jaredwy <jared.wyles@gmail.com>
2011-03-17 21:17:39 +1100

    adding some javascript to add comments

--- static/js/comment.js
+++ static/js/comment.js
@@ -2,5 +2,41 @@
     Raphael.eve.on("overlayer.drawing.stop",function(opts) {
         var commentDialog = document.getElementById("comment-dialog");
         commentDialog.style.display = "block";
+        var button = document.getElementById("comment-submit");
+        button.onsubmit = function(e) {
+            e.preventDefault();
+            var template =$("script[name='comment']")[0].text;
+            var comment = {
+                "name": "something",
+                "version": "1.1",
+                "text" : document.getElementById("comment").value
+            }
+            $.tmpl( template , comment).appendTo("#annotation1")    
+            
+            return false;
+        }    
     });
+    
+    $(document).ready(function() {
+       $(".reply").click(function(e) {
+            e.preventDefault();
+            var commentDialog = document.getElementById("comment-dialog");
+            commentDialog.style.display = "block";
+            var button = document.getElementById("comment-submit");
+            var insertAfter = $(this).closest(".comment");
+            button.onsubmit = function(e) {
+                e.preventDefault();
+           
+                var template =$("script[name='comment']")[0].text;
+                var comment = {
+                    "name": "something",
+                    "version": "1.1",
+                    "text" : document.getElementById("comment").value
+                }
+                var a =  $.tmpl( template , comment);
+                insertAfter.after(a);
+            }
+       });
+    }); 
+    
 })();
